2cb37ceb71734a65274c160d1187bfab18ebaf52
Jonas Rylund Glesaaen <jonas@glesaaen.com>
2016-02-05 12:54:48 +0100

    Squashed 'C++_User_Group_Meetings/2016_Thinking_with_templates/' content from commit c1c10fc
    git-subtree-dir: C++_User_Group_Meetings/2016_Thinking_with_templates
    git-subtree-split: c1c10fc6f05aa800f482aed1c1c8364b67f48c92

---
+++ Examples/ex4_crtp.cpp
@@ -0,0 +1,51 @@
+/*
+ * Created: 04-02-2016
+ * Modified: Fri 05 Feb 2016 10:52:38 CET
+ * Author: Jonas R. Glesaaen (jonas@glesaaen.com)
+ */
+
+#include<iostream>
+
+template <typename T>
+class Base
+{
+public:
+  virtual double foo()
+  {
+    std::cout << "Casting" << std::endl;
+    return static_cast<T*>(this)->foo();
+  }
+};
+
+class Cl1 : public Base<Cl1>
+{
+public:
+  double foo()
+  {
+    return 4;
+  }
+};
+
+class Cl2 : public Base<Cl2>
+{
+public:
+  double foo()
+  {
+    return 5;
+  }
+};
+
+template <typename Type>
+double call(Base<Type> x)
+{
+  return x.foo();
+}
+
+int main(int, char**)
+{
+  auto x = Cl1 {};
+  auto y = Cl2 {};
+
+  std::cout << call(x) << "\n";
+  std::cout << call(y) << "\n";
+}
---
+++ Examples/ex5_counter.cpp
@@ -0,0 +1,37 @@
+/*
+ * Created: 05-02-2016
+ * Modified: Fri 05 Feb 2016 11:26:13 CET
+ * Author: Jonas R. Glesaaen (jonas@glesaaen.com)
+ */
+
+#include<iostream>
+
+template <typename Type>
+class Counter
+{
+public:
+  Counter()
+  {
+    ++instances;
+  }
+
+  ~Counter()
+  {
+    --instances;
+  }
+
+  static unsigned instances;
+};
+
+class A : public Counter<A> {};
+class B : public Counter<B> {};
+
+template <typename Type>
+unsigned Counter<Type>::instances = 0;
+
+int main(int, char**)
+{
+  A a1, a2, a3, a4;
+  B b1, b2;
+  std::cout << Counter<B>::instances << std::endl;
+}
